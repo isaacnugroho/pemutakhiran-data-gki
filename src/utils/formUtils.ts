c9d6df98736c0d6a6e877d3d1aa537131e37adf1
Isaac A. Nugroho <isaac.nugroho@gmail.com>
2025-04-03 13:40:18 +0700

    fix gender prefill

--- src/utils/formUtils.ts
+++ src/utils/formUtils.ts
@@ -534,26 +534,12 @@ export const handleFormSubmit = (): void => {
   //&entry.1486481296=kelurahan
   //&entry.1101194549=12345
   
-  // &entry.1276998498=nama
-  // &entry.932581507=phone
-  // &entry.1552198038=birthPlace
-  // &entry.946724977=2025-05-05
-  // &entry.157174969=Laki-laki
-  // &entry.1949922653=AB%2B
-  // &entry.670185927=address
-  // &entry.1226075221=rtrw
-  // &entry.1471260286=provinsi
-  // &entry.198554559=kota
-  // &entry.347596421=kecamatan
-  // &entry.687992146=kelurahan
-  // &entry.56586160=12345
-  // Construct the prefilled URL
   const prefilledUrl = `https://docs.google.com/forms/d/e/${PREFILLED_FORM_ID}/viewform?usp=pp_url` +
     `&entry.113342069=${encodeURIComponent(name)}` +
     // `&entry.216880000=${encodeURIComponent(phone)}` +
     `&entry.687432613=${encodeURIComponent(birthPlace)}` +
     `&entry.1450724760=${encodeURIComponent(birthDate)}` +
-    `&entry.992412807=${encodeURIComponent(gender)}` +
+    `&entry.992412807=${encodeURIComponent(gender === 'Laki-laki' ? 'LAKI-LAKI' : 'PEREMPUAN')}` +
     `&entry.1152849548=${encodeURIComponent(bloodType)}` +
     `&entry.1792121930=${encodeURIComponent(address)}` +
     `&entry.1886783988=${encodeURIComponent(rtrw)}` +
