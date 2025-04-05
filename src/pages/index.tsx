0e68fda7e0b599de636c0acc7ab81502b7a6407a
Isaac A. Nugroho <isaac.nugroho@gmail.com>
2025-04-05 17:23:12 +0700

    switch label position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -152,8 +152,8 @@ export default function Home() {
                       <div className="input-field col s12">
                         <textarea id="address" className="materialize-textarea validate"></textarea>
                         <label htmlFor="address">Alamat Lengkap*</label>
-                        <span className="helper-text2">Harap isi alamat dengan dimulai dari nama perumahan/komplek</span>
                         <span className="helper-text"></span>
+                        <span className="helper-text2">Harap isi alamat dengan dimulai dari nama perumahan/komplek</span>
                       </div>
                     </div>
 
