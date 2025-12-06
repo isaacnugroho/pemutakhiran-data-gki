f009d30dd52209360ff42c53faa85137ef59cc0a
Isaac A. Nugroho <isaac.nugroho@gmail.com>
2025-12-06 21:39:54 +0700

    FIX: fixed vulnerability

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   env: {
     PREFILLED_FORM_ID: process.env.PREFILLED_FORM_ID || 'not_the_actual_PREFILLED_FORM_ID',
   },
-  // This allows Next.js to serve static files from the public directory
-  publicRuntimeConfig: {
-    staticFolder: '/public',
-  },
 }
 
 module.exports = nextConfig
